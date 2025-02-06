3960717c33c2009b53c2112fdf27fefbe56bfbb9
XieZongChen <952937570@qq.com>
2025-02-06 14:58:18 +0800

    feat: 搭建测试环境

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,7 @@
-const content = <div>
-    <Test>test</Test>
-    <a href="xxx">link</a>
-</div>
+const content = (
+  <div>
+    <a href='xxx'>link</a>
+  </div>
+);
+
+console.log(JSON.stringify(content, null, 2));
