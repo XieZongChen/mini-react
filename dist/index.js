0f64ac663c9f3cc77e1ae63f76a1c6fc492fa5b7
XieZongChen <952937570@qq.com>
2025-02-06 14:43:19 +0800

    feat: init

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const content = React.createElement("div", null,
+    React.createElement(Test, null, "test"),
+    React.createElement("a", { href: "xxx" }, "link"));
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+const content = <div>
+    <Test>test</Test>
+    <a href="xxx">link</a>
+</div>
