f3125dc88cd2185e4e1b021124bd805c7dcda58b
XieZongChen <952937570@qq.com>
2025-02-07 10:10:54 +0800

    feat: dom 的创建和更新逻辑

--- src/mini-react.js
+++ src/mini-react.js
@@ -105,6 +105,66 @@ function updateHostComponent(fiber) {
   reconcileChildren(fiber, fiber.props.children);
 }
 
+/**
+ * 创建 dom
+ */
+function createDom(fiber) {
+  // 根据不同节点类型用不同方式创建 dom
+  const dom =
+    fiber.type == 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type);
+
+  updateDom(dom, {}, fiber.props);
+
+  return dom;
+}
+
+const isEvent = (key) => key.startsWith('on'); // 是否是事件
+const isProperty = (key) => key !== 'children' && !isEvent(key); // 是否是属性
+const isNew = (prev, next) => (key) => prev[key] !== next[key]; // 是否是新的
+const isGone = (prev, next) => (key) => !(key in next); // 是否是删除的
+
+/**
+ * 更新 dom
+ * - 主要是更新 dom 上的 props，包括属性和事件
+ */
+function updateDom(dom, prevProps, nextProps) {
+  // 删除旧的或有更改的 event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+  // 删除旧 properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = '';
+    });
+
+  // 设置新的或者有更改的 properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+
+  // 添加 event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 const MiniReact = {
   createElement,
 };
